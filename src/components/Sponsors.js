89ae4071f84f5759cd387def6a361943e39d87a9
Kurt Gardiner <k.a.gardiner@gmail.com>
2018-10-09 16:34:41 +1100

    :alien: Comments out hudson and pagerduty for now

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -12,9 +12,9 @@ import NewyTechPeople from "../assets/images/logo-newytechpeople.jpg";
 import Vibrato from "../assets/images/vibrato-logo.png";
 import LWB from "../assets/images/LWB.png";
 import DIUS from "../assets/images/dius.png";
-import Hudson from "../assets/favicon.png";
 import SumoLogic from "../assets/images/logo-sumologic.png";
-import PagerDuty from "../assets/favicon.png";
+{/* import PagerDuty from "../assets/images/PagerDuty_logo_GREEN.png"; */}
+{/* import Hudson from "../assets/favicon.png"; */}
 
 const Sponsors = () => (
   <section id="sponsors">
@@ -84,6 +84,7 @@ const Sponsors = () => (
               </div>
             </div>
 
+            {/* Haven't received confirmation yet
             <div className="columns portfolio-item">
               <div className="item-wrap gold">
                 <a href="https://www.pagerduty.com/" title="PagerDuty">
@@ -91,6 +92,7 @@ const Sponsors = () => (
                 </a>
               </div>
             </div>
+           */}
           </div>
         </div>
       </div>
@@ -117,6 +119,7 @@ const Sponsors = () => (
               </div>
             </div>
 
+            {/*
             <div className="columns portfolio-item">
               <div className="item-wrap gold">
                 <a href="https://au.hudson.com/" title="Hudson">
@@ -124,6 +127,7 @@ const Sponsors = () => (
                 </a>
               </div>
             </div>
+            */}
 
             <div className="columns portfolio-item">
               <div className="item-wrap gold">
